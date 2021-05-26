48582cbc90767194983f76c19553b7d6579662cd
stillgood96 <a787574@naver.com>
2021-05-26 09:15:12 +0900

    search : add youtube service -Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,25 @@ import "@fortawesome/fontawesome-free/js/all.js";
 import { useEffect, useState } from "react";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyB_m_LruSxHt_A4Bm7FHA76NL-0fDPxf9E&key=AIzaSyB_m_LruSxHt_A4Bm7FHA76NL-0fDPxf9E`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .search(query) //
+      .then((videos) => {
+        setVideos(videos);
+      });
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyB_m_LruSxHt_A4Bm7FHA76NL-0fDPxf9E&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyB_m_LruSxHt_A4Bm7FHA76NL-0fDPxf9E&key=AIzaSyB_m_LruSxHt_A4Bm7FHA76NL-0fDPxf9E",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((videos) => {
+        setVideos(videos);
+      });
   }, []);
+
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
+import YouTube from "./service/youtube";
+
+const youtube = new YouTube(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById("root")
 );
