48580ba5e962c46f7eb7d4c0eadf1315e43dfc03
stillgood96 <a787574@naver.com>
2021-05-28 02:55:26 +0900

    Video detail and change list according to the situation

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import SearchHeader from "./components/search_header/search_header.jsx";
 import "@fortawesome/fontawesome-free/js/all.js";
 import { useEffect, useState } from "react";
 import VideoList from "./components/video_list/video_list";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
 
   const search = (query) => {
     youtube
@@ -26,7 +32,20 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import styles from "./video_item.module.css";
 
-const VideoItem = (props) => (
-  <li className={styles.container}>
-    <div className={styles.video}>
-      <img
-        src={props.video.snippet.thumbnails.medium.url}
-        alt="video thumbnail"
-        className={styles.thumbnail}
-      ></img>
-      <div className={styles.metaData}>
-        <p className={styles.title}>{props.video.snippet.title}</p>
-        <p className={styles.channel}>{props.video.snippet.channelTitle}</p>
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayType = display === "list" ? styles.list : styles.grid;
+  return (
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
+      <div className={styles.video}>
+        <img
+          src={snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+          className={styles.thumbnail}
+        ></img>
+        <div className={styles.metaData}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default VideoItem;
