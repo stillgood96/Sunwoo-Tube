85f14b315adb408f07bbf7d2658e928f9646bf28
stillgood96 <a787574@naver.com>
2021-06-01 16:03:21 +0900

    use axios & dependency injection

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 import YouTube from "./service/youtube";
+import axios from "axios";
 
-const youtube = new YouTube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const httpClient = axios.create({
+  baseURL: "https://youtube.googleapis.com/youtube/v3",
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+});
+const youtube = new YouTube(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,28 +1,43 @@
 class YouTube {
-  constructor(key) {
-    this.key = key;
-    this.getRequestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+  constructor(httpClient) {
+    this.youtube = httpClient;
   }
 
   async mostPopular() {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?key=${this.key}&part=snippet&chart=mostPopular&maxResults=25`,
-      this.getRequestOptions
-    );
-    const result_1 = await response.json();
-    return result_1.items;
+    const response = await this.youtube.get("videos", {
+      params: {
+        part: "snippet",
+        chart: "mostPopular",
+        maxResults: 25,
+      },
+    });
+    return response.data.items;
+    // const response = await fetch(
+    //   `https://youtube.googleapis.com/youtube/v3/videos?key=${this.key}&part=snippet&chart=mostPopular&maxResults=25`,
+    //   this.getRequestOptions
+    // );
+    // const result_1 = await response.json();
+    // return result_1.items;
   }
 
   async search(query) {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-      this.requestOptions
-    );
-    const result_1 = await response.json();
-    return result_1.items.map((item) => ({ ...item, id: item.id.videoId }));
+    const response = await this.youtube.get("search", {
+      params: {
+        part: "snippet",
+        chart: "mosPopular",
+        type: "video",
+        maxResults: 25,
+        q: query,
+      },
+    });
+    return response.data.items;
+    //   const response = await fetch(
+    //     `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+    //     this.requestOptions
+    //   );
+    //   const result_1 = await response.json();
+    //   return result_1.items.map((item) => ({ ...item, id: item.id.videoId }));
+    // }
   }
 }
 export default YouTube;
